95806b881aaf03c7bd8e82918c916033efaeca8a
truark21 <Truark21@JARVIS.local>
2019-11-18 21:19:23 -0500

    Laying out plans

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,8 @@
-#this is just a test
+#The goal of this program is to allow a player to play against a computer in a game of hangman.
+#Below are some goals on what this program should do
+#
+# -ask the player for a difficulty
+# -have a list of words based off difficulty and chooses at random
+# -draws a stick figure whenever a letter is guessed wrong
+# -state that there are no more vowels
+
