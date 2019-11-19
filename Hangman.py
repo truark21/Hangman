fa3e171349b0c4f73dd51a1ddfcc44b0c5f7810d
truark21 <Truark21@JARVIS.local>
2019-11-18 21:43:40 -0500

    Asking for difficulty was added

--- Hangman.py
+++ Hangman.py
@@ -6,3 +6,20 @@
 # -draws a stick figure whenever a letter is guessed wrong
 # -state that there are no more vowels
 
+def AskingForDiff():
+    print("Please select a number for the difficulty. Below is the rating.")
+    print("1: 3 or 4 letter words")
+    print("2: 5 or 6 letter words")
+    print("3: 7 or 8 letter words")
+    while True:
+        try: 
+            difficulty = int(input("Select between 1-3: "))
+        except ValueError:
+            print("Value must be numeric.")
+        if difficulty > 3:
+            print("Try again.")
+        else:
+            break
+    
+
+AskingForDiff()
